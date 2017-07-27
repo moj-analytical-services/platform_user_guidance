a9e7cff145146810142a6d30917228e6f2af60e2
willbowditch <will@bowditch>
2017-07-27 11:54:55 +0100

    better link checks

--- _build.sh
+++ _build.sh
@@ -2,4 +2,4 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
 # Check for broken links
 npm install -g broken-link-checker-local
-blcl docs/ -er
+blcl /docs -re --filter-level 3
